4df66f3e6f96ff1a0cca462091437d74b9111b13
lucasrsouza <lucas.souza@omotor.com.br>
2019-10-01 00:13:37 -0300

    Changing in routes and delete method

--- main.go
+++ main.go
@@ -3,19 +3,21 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
 
-type Pets []Pet
+var pets []Pet
 
 type Pet struct {
-	Name string `json:"Name"`
-	Age int `json:"Age"`
+	ID          string `json:"id,omitempty"`
+	Name        string `json:"Name"`
+	Age         int    `json:"Age"`
 	Description string `json:"Description"`
 }
 
-func savePet( w http.ResponseWriter, r *http.Request){
+func savePet(w http.ResponseWriter, r *http.Request) {
 	var pet Pet
 	err := json.NewDecoder(r.Body).Decode(&pet)
 
@@ -27,27 +29,41 @@ func savePet( w http.ResponseWriter, r *http.Request){
 
 }
 
-func getPets(w http.ResponseWriter, r *http.Request){
-	pets := Pets{
-		Pet{Name:"Nino", Age:17, Description:"My Best Dog!"},
-	}
+func getAllPets(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(pets)
 }
 
-func home(w http.ResponseWriter, r *http.Request){
+func getPet(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome home!")
 }
 
-func handleRequest() {
-	http.HandleFunc("/", home)
-	http.HandleFunc("/save/pet", savePet)
-	http.HandleFunc("/pets", getPets)
+func deletePet(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for index, item := range pets {
+		if item.ID == params["id"] {
+			pets = append(pets[:index], pets[index+1:]...)
+			break
+		}
+		json.NewEncoder(w).Encode(pets)
+	}
+}
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+func handleRequest() {
+	//Routes
+	router := mux.NewRouter()
+
+	router.HandleFunc("/pet/{id}", getPet).Methods("GET")
+	router.HandleFunc("/pet", getAllPets).Methods("GET")
+	router.HandleFunc("/pet", savePet).Methods("POST")
+	router.HandleFunc("/pet/{id}", deletePet).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func main() {
 	fmt.Println("API Started")
 	handleRequest()
-}
 
+	pets = append(pets, Pet{ID: "1", Name: "John", Age: 12, Description: "Border Collie"})
+	pets = append(pets, Pet{ID: "2", Name: "Toby", Age: 12, Description: "Labrador"})
+}
